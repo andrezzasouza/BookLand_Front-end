78ef355bd3b3e47faf8ccd8001017d45e5b0de65
PMafra <pmafrarm@gmail.com>
2021-11-07 16:18:56 -0300

    Set up folders and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
+import GlobalStyle from './assets/styles/GlobalStyle';
+import UserContext from './store/UserContext';
+import TransitionStyle from './assets/styles/TransitionStyle';
 
-function App() {
+export default function App() {
+  const [token, setToken] = useState('');
+
   return (
-    <>
-    </>
+    <UserContext.Provider value={{ token, setToken }}>
+      <Router>
+        <GlobalStyle />
+        <TransitionStyle />
+        <AppRoutes />
+      </Router>
+    </UserContext.Provider>
   );
 }
-
-export default App;
---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_URL = "";
+
+const createHeaders = (token) => {
+    return { 
+        headers: { 
+            Authorization: `Bearer ${token}` 
+        } 
+    };
+}
+
+//const sendNewRecord = ( token, body ) => axios.post(`${API_URL}/records`, body, createHeaders(token));
+
+export {
+
+}
